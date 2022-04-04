fd7b4f1ea40a04b4f104f24763bc17b5f09fd201
PMBytner <pawel.m.bytner@gmail.com>
2022-04-04 11:05:18 +0200

    dodanie widoku szczegółów

---
+++ Szczegóły.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NET_INIS4_PR2._2_z4
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Szczegóły.xaml
+    /// </summary>
+    public partial class Szczegóły : Window
+    {
+        Osoba osoba;
+        public Szczegóły(Osoba osoba)
+        {
+            this.osoba = osoba;
+            DataContext = osoba;
+            InitializeComponent();
+        }
+    }
+}
