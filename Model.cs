5e5a53493b9f20ec6b9e974046f236ae45af8d0e
PMBytner <pawel.m.bytner@gmail.com>
2022-04-04 11:07:37 +0200

    korekta listy danych i otwieranie szczegółów

--- Model.cs
+++ Model.cs
@@ -8,9 +8,17 @@ namespace NET_INIS4_PR2._2_z4
 {
     class Model
     {
-        public IEnumerable<string> Lista = new LinkedList<string>(new string[] {
-        
-        
+        public IEnumerable<Osoba> Lista { get; set; } = new LinkedList<Osoba>(new Osoba[] {
+            new Osoba() {Imię = "Adam", Nazwisko = "Wiśniewski", Wiek = 30},
+            new Osoba() {Imię = "Marian", Nazwisko = "Dąbrowski", Wiek = 29},
+            new Osoba() {Imię = "Julia", Nazwisko = "Jabłońska", Wiek = 15},
+            new Osoba() {Imię = "Sylwia", Nazwisko = "Sosnowska", Wiek = 25},
         });
+
+        internal void OtwórzSzczegóły(Osoba wybrany)
+        {
+            Szczegóły szczegóły = new Szczegóły(wybrany);
+            szczegóły.Show();
+        }
     }
 }
