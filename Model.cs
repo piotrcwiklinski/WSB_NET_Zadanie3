6e1818db8be35a6b7cf769d9745c88ef326ca2a1
PMBytner <pawel.m.bytner@gmail.com>
2022-04-04 10:08:58 +0200

    Dodaj pliki projektów.

---
+++ Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_INIS4_PR2._2_z4
+{
+    class Model
+    {
+        public IEnumerable<string> Lista = new LinkedList<string>(new string[] {
+        
+        
+        });
+    }
+}
---
+++ Osoba.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_INIS4_PR2._2_z4
+{
+    class Osoba : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly static Dictionary<string, string[]> relatedProperties = new Dictionary<string, string[]>()
+        {
+            ["Imię"] = new string[] {"ImięNazwisko"},
+            ["Nazwisko"] = new string[] {"ImięNazwisko"}
+        };
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            foreach(string relatedProperty in relatedProperties[propertyName])
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(relatedProperty));
+        }
+
+        static uint następneID = 0;
+        string
+            imię,
+            nazwisko
+            ;
+
+        public string Imię {
+            get => imię;
+            set
+            {
+                imię = value;
+                OnPropertyChanged();
+                OnPropertyChanged("ImięNazwisko");
+            }
+        }
+
+        public string Nazwisko { get => nazwisko; set => nazwisko = value; }
+        public string ImięNazwisko { get => $"{imię} {nazwisko}"; }
+        public uint Wiek { get; set; } = 0;
+        public uint ID { get; } = następneID++;
+    }
+}
